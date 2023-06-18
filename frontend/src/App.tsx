a3c15475ca8290f90160b4875002b4108a658df5
Arif Shariati <mohammad.arif.fast@hotmail.com>
2023-06-18 12:34:19 +0200

    Added AppRouter usage in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { FC } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import AppRouter from 'Router';
 import theme from 'theme';
 
 const App: FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div>App</div>
+      <AppRouter />
     </ThemeProvider>
   );
 };
