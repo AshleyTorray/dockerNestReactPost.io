bbabd37461030c3384a1d0aa2d04f9acf4600924
Arif Shariati <mohammad.arif.fast@hotmail.com>
2023-06-18 12:03:39 +0200

    refactored theme related files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
-function App() {
+import { FC } from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from 'theme';
+
+const App: FC = () => {
   return (
-    <div className="App">
-      <h1>Hi</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>App</div>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
